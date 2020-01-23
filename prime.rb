441f45c7e27eeed1f4d74f331ef44a9b3bcae6d8
Danny Soto <87dsoto@gmail.com>
2020-01-23 16:43:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  elsif num == 2 
+    return true
+  else
+    x = 2
+    while x < num
+      if num % x == 0
+        return false 
+      end
+      x += 1
+    end
+    return true
+  end
+end 
