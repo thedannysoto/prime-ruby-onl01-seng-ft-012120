278cb491ec93e7c8e72b3fd62362d9ea388d926c
Danny Soto <87dsoto@gmail.com>
2020-01-23 16:43:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,16 @@
 def prime?(num)
-  
+  if num < 2
+    return false
+  elsif num == 2 
+    return true
+  else
+    x = 2
+    while x < num
+      if num % x == 0
+        return false 
+      end
+      x += 1
+    end
+    return true
+  end
+end 
